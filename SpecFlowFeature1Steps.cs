f8d7109ab3ee8b72c0eea75c8ac76b8c5b26f71d
Bibby <bibbynet@gmail.com>
2014-09-26 12:15:19 +0800

    add specrun for specflow

---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
